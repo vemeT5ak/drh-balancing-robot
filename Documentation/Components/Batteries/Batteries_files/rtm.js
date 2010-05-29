ff8a91ff5ff494928fc260647442442b6d18125c
rainer.hessmer <rainer.hessmer@f0d78b29-9a4e-05e9-2f12-30d8730bb76c>
2010-05-29 14:54:50 +0000

    Info about components used in the robot.

---
+++ Documentation/Components/Batteries/Batteries_files/rtm.js
@@ -0,0 +1,14 @@
+try{vjo.dsf.assembly.VjClientAssembler._callback0([
+{"id":"699","type":"none","width":"","height":"","content":""}, 
+{"id":"973","type":"none","width":"0","height":"0","content":""}, 
+{"id":"974","type":"none","width":"0","height":"0","content":""}, 
+{"id":"813","type":"none","width":"0","height":"0","content":""}, 
+{"id":"825","type":"html","width":"300","height":"100","content":"<html><body>\n  <a href=\"http://srx.main.ebayrtm.com/clk?RtmClk&lid=624988&m=136364&pi=4340&i=-9999&g=7b2badc41210a0aad1e4c7c4fdf758f6&uf=0\" alt=\"6 months special financing\" target=\"_top\">\n  <img border=\"0\" src=\"http://rtm.ebaystatic.com/0/RTMS/Image/PROD-MasterCardV2-Nov-Q409-300x100.gif\" alt=\"6 months special financing\" />\n  </a>\n</body></html>"}, 
+{"id":"827","type":"none","width":"0","height":"0","content":""}, 
+{"id":"829","type":"none","width":"","height":"","content":""}, 
+{"id":"283","type":"html","width":"1","height":"1","content":"<html>\n<head/>\n<body style=\"margin:0; padding:0;\">\n\n<img src=\"http://pics.ebaystatic.com/aw/pics/s.gif\" onLoad=\"setTimeout(function() { var d = document.getElementById(\'rtm_html_283\'); d.innerHTML=\'\'; }, 1000);\"></img>\n<iframe width=\"1\" height=\"1\" scrolling=\"no\" frameborder=\"no\" marginheight=\"0\" marginwidth=\"0\" border=\"0\" src=\"http://tags.bluekai.com/site/50?ret=html&limit=4&url=cgi.ebay.com&\nphint=kw%3D&phint=kh%3D2A236136F467A0A37A75FB94BE3A2D8C&phint=eid%3D283&phint=oi%3Dtrue&uhint=zip%3D0&phint=tcat%3D48479&phint=asp%3D\"/>\n</body>\n</html>"}, 
+{"id":"280","type":"none","width":"0","height":"0","content":""}, 
+{"id":"433","type":"html","width":"112","height":"22","content":"<html><body>\n  <a href=\"http://srx.main.ebayrtm.com/clk?RtmClk&lid=338626&m=60682&pi=4340\" target=\"_top\">\n  <img border=\"0\" src=\"http://rtm.ebaystatic.com/0/RTMS/Image/ADVT-SCHeaderNonTB_Q407-112x22.gif\" />\n  </a>\n</body></html>"}, 
+{"id":"876","type":"none","width":"0","height":"0","content":""}, 
+{"id":"912","type":"none","width":"0","height":"0","content":""}
+]);}catch(e){}
