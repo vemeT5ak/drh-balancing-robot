d2cbae6ee606bb91771de9f5312335adbe0a65da
rainer.hessmer <rainer.hessmer@f0d78b29-9a4e-05e9-2f12-30d8730bb76c>
2010-05-30 18:51:33 +0000

    The main window is now composed of two separate panels holding the tilt angle plot and speed plot respectively. The code for setting the PID parameters of the speed controller is in place.

--- PythonClient/RobotController/src/Model/MainModel.py
+++ PythonClient/RobotController/src/Model/MainModel.py
@@ -23,7 +23,8 @@ class MainModel(object):
         '''
         
         self.SerialPort = serialPort
-        self.Coefficients = [0,0,0,0] # Coefficients K1, .. K4
+        self.SpeedControlParams = {'P':0, 'I':0, 'D':0} # P,I,D parameters for the speed controller
+        self.Coefficients = {'K1':0, 'K2':0, 'K3':0, 'K4':0} # Coefficients K1, .. K4
         
         self._Settings = Settings(self)
         self._Settings.Load()
@@ -35,6 +36,19 @@ class MainModel(object):
     
     def OnCoefficientsReceived(self, coefficients):
         self.Coefficients = coefficients
+        
+    def SetSpeedControlParams(self, params):
+        '''
+        params is expected to a tuple holding three values that can be converted to floats representing the P,I,D parameters
+        '''
+        
+        floatParams = tuple(float(param) for param in params)
+        
+        self.SpeedControlParams['P'] = floatParams[0]
+        self.SpeedControlParams['I'] = floatParams[1]
+        self.SpeedControlParams['D'] = floatParams[2]
+        
+        self._DataAdapter.SendSpeedControllerParams(floatParams)
     
     def Close(self):
         self._DataAdapter.Stop()
---
+++ PythonClient/RobotController/src/UI/TiltPlotPanel.py
@@ -0,0 +1,115 @@
+'''
+Created on May 29, 2010
+
+@author: Dr. Rainer Hessmer
+'''
+
+import wx.lib.sized_controls as sc
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigureCanvas
+import matplotlib.dates as mpldates 
+import pylab
+
+class TiltPlotPanel(sc.SizedPanel):
+    '''
+    classdocs
+    '''
+
+    def __init__(self, parent, maxAgeBuffer, *args, **kwargs):
+        """Create the DemoPanel."""
+        sc.SizedPanel.__init__(self, parent, *args, **kwargs)
+
+        #self.parent = parent
+        
+        self._TimeStamps = []
+        self._Values1 = []
+        self._Values2 = []
+        
+        self._ExtractPlotData(maxAgeBuffer)
+
+        self._InitializePlot()
+
+    def _InitializePlot(self):
+        self._DPI = 100
+        self._Figure = Figure(dpi=self._DPI)
+
+        self._Plot = self._Figure.add_subplot(111)
+        self._Plot.set_axis_bgcolor('black')
+        self._Plot.set_title('Tilt Angle', size=12)
+        self._Plot.grid(True, color='gray')
+        
+        pylab.setp(self._Plot.get_xticklabels(), fontsize=8)
+        pylab.setp(self._Plot.get_yticklabels(), fontsize=8)
+        
+        formatter = mpldates.DateFormatter('%M:%S') 
+        self._Plot.xaxis.set_major_formatter(formatter)
+
+
+        # plot the data as a line series, and save the reference 
+        # to the plotted line series
+        #
+        self._Traces = []
+        trace = self._Plot.plot(
+            self._TimeStamps,
+            self._Values1,
+            linewidth=1,
+            color=(1, 0, 0),
+            )[0]
+        self._Traces.append(trace)
+
+        trace = self._Plot.plot(
+            self._TimeStamps,
+            self._Values2,
+            linewidth=1,
+            color=(0, 1, 0),
+            )[0]
+            
+        self._Traces.append(trace)
+        
+        legend = self._Plot.legend(('Acceleration', 'Kalman'), shadow=False, labelspacing=0.001)
+        for text in legend.get_texts():
+            text.set_fontsize('x-small')    # the legend text fontsize
+
+
+        self._FigureCanvas = FigureCanvas(self, -1, self._Figure)
+        self._FigureCanvas.SetSizerProps(expand=True, proportion=1)
+        
+    def Refresh(self, maxAgeBuffer, now):
+        self._ExtractPlotData(maxAgeBuffer)
+        self._DrawPlot(maxAgeBuffer, now)
+        
+    def _ExtractPlotData(self, maxAgeBuffer):
+
+        del self._TimeStamps[:]
+        del self._Values1[:]
+        del self._Values2[:]
+
+        for timeStampedValue in maxAgeBuffer:
+            self._TimeStamps.append(timeStampedValue.TimeStamp)
+            self._Values1.append(timeStampedValue.Value[0])
+            self._Values2.append(timeStampedValue.Value[1])       
+
+    def _DrawPlot(self, maxAgeBuffer, now):
+        """ Redraws the plot
+        """
+        
+        #print 'redrawing'
+        #print self._TimeStamps
+        #print self._Values1
+        
+        self._Plot.set_xbound(lower=now - maxAgeBuffer.MaximumAge, upper=now)
+        self._Plot.set_ybound(lower=-10.0, upper=10.0)
+
+
+        self._Traces[0].set_xdata(self._TimeStamps)
+        self._Traces[0].set_ydata(self._Values1)
+
+        self._Traces[1].set_xdata(self._TimeStamps)
+        self._Traces[1].set_ydata(self._Values2)
+
+#        self.plot_data.set_xdata(np.arange(len(self.data)))
+#        self.plot_data.set_ydata(np.array(self.data))
+        
+        if len(self._TimeStamps) > 0:
+            self._FigureCanvas.draw()
+ 
