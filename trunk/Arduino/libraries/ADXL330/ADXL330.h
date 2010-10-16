838ba5a352fdf8943640d600782baec88a553a56
rainer.hessmer <rainer.hessmer@f0d78b29-9a4e-05e9-2f12-30d8730bb76c>
2010-10-16 02:57:18 +0000

    Folding the code into the ADXL330.h file.

--- trunk/Arduino/libraries/ADXL330/ADXL330.h
+++ trunk/Arduino/libraries/ADXL330/ADXL330.h
@@ -8,21 +8,80 @@
 #define ADXL330_h
 
 #include "WProgram.h"
+#include "ADXL330.h"
+#include <math.h>
+
+// My rough measurements indicate the following analog values:
+//      -g  +g  delta mean
+// x	620 416 -204  518
+// y    620 411 -209  515.5
+// z    604 410 -194  507
 
 class ADXL330
 {
-  public:
-    ADXL330(int xPin, int yPin, int zPin);
-	void Update();
-	float XAcceleration, YAcceleration, ZAcceleration;  // acceleration in units of g
-	float TotalAcceleration; // total acceleration in units of g
+	public:
+		float XAcceleration, YAcceleration, ZAcceleration;  // acceleration in units of g
+		float TotalAcceleration; // total acceleration in units of g
 
-	// calculated angles
-	float GetXYAngleRad();
-	float GetXZAngleRad();
-	float GetYZAngleRad();  
-  private:
-    int _xPin, _yPin, _zPin;
+		ADXL330(int xPin, int yPin, int zPin)
+		{
+			_xPin = xPin;
+			_yPin = yPin;
+			_zPin = zPin;
+
+			analogReference(EXTERNAL);
+		}
+
+		void Update()
+		{
+			double xRaw = analogRead(_xPin);
+			double yRaw = analogRead(_yPin);
+			double zRaw = analogRead(_zPin);
+
+			/*Serial.print("double");
+			Serial.print("\t");
+			Serial.print(xRaw);
+			Serial.print("\t");
+			Serial.print(yRaw);
+			Serial.print("\t");
+			Serial.print(zRaw);
+			Serial.println();*/
+
+			// Calibrate
+			XAcceleration = (xRaw - 518.0)   / -204.0 * 2.0;
+			YAcceleration = (yRaw - 515.5) / -209.0 * 2.0;
+			ZAcceleration = (zRaw - 507.0)   / -194.0 * 2.0;
+
+			TotalAcceleration = sqrt(XAcceleration * XAcceleration + YAcceleration * YAcceleration + ZAcceleration * ZAcceleration);
+
+			/*Serial.print("double");
+			Serial.print("\t");
+			Serial.print(XAcceleration);
+			Serial.print("\t");
+			Serial.print(YAcceleration);
+			Serial.print("\t");
+			Serial.print(ZAcceleration);
+			Serial.println();*/
+		}
+
+		// calculated angles
+		float GetXYAngleRad()
+		{
+			return atan2(YAcceleration, XAcceleration);
+		}
+
+		float GetXZAngleRad()
+		{
+			return atan2(ZAcceleration, XAcceleration);
+		}
+
+		float GetYZAngleRad()
+		{
+			return atan2(ZAcceleration, YAcceleration);
+		}
+
+	private:
+		int _xPin, _yPin, _zPin;
 };
 
 #endif
